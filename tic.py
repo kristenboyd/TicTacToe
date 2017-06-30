d1bd7e123ea0d334f06a8a183834c0842e7b4138
kristenboyd <kpboyd95@gmail.com>
2017-06-30 11:43:00 -0400

    Add files via upload

---
+++ tic.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+board = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7','8','9']
+]
+
+def print_grid():
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+                print (board[i][j], end=" | ")
+        print ("\n___________")
+
+
+def init_game():
+    print ("Welcome to Tic Tac Toe" + "\n Player One will be 'X'"
+    + "\n Player Two will be 'O'")
+
+    Player1 = raw_input("Enter in a position from 1-9  ")
+    type(Player1)
+    Player2 = raw_input("Enter in a position from 1-9  ")
+    type(Player2)
+    print_grid()
+
+init_game()
