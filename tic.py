b1c9ab5e0b9cc8948d9da1b9ad2435e0262f29d8
kristenboyd <kpboyd95@gmail.com>
2017-06-30 22:34:45 -0400

    revisions

--- tic.py
+++ tic.py
@@ -6,13 +6,6 @@ board = [
     ['7','8','9']
 ]
 
-
-def draw():
-        print(board[0], board[1], board[2])
-        print(board[3], board[4], board[5])
-        print(board[6], board[7], board[8])
-        print()
-
 def print_grid():
     for i in range(len(board)):
         for j in range(len(board[i])):
@@ -25,10 +18,8 @@ def init_game():
     + "\n Player Two will be 'O'")
 
     Player1 = raw_input("Enter in a position from 1-9  ")
-    type(Player1)
     Player2 = raw_input("Enter in a position from 1-9  ")
-    type(Player2)
     print_grid()
-    print "change"
+
 
 init_game()
