b5a890816fde0f982e8891ae444feb781d4b6eb9
kristenboyd <kpboyd95@gmail.com>
2017-10-09 10:22:49 -0400

    Add files via upload
    Can now tell user who won the game

--- tic.py
+++ tic.py
@@ -8,10 +8,8 @@ board = [
 
 
 
-def print_intro():
-    "Use the board to choose you position number, \nyou can only choose numbers 1-9"
-
 def print_grid():
+    "Use the board to choose you position number, \nyou can only choose numbers 1-9"
     for i in range(len(board)):
         for j in range(len(board[i])):
                 print (board[i][j], end=" | ")
@@ -88,38 +86,98 @@ def get_colum_index(input):
 
 def start_game():
     running = True
-    playerturn  = 1
+    playerturn = 1
     while running:
         if playerturn == 1:
-            postion = get_position(playerturn)
-            print (position)
-            mark_position(playerturn,input)
-            print_grid()
+            position = get_position(playerturn)
+            mark_position(playerturn,position)
 
-            running = False
-    print ("game over")
+            print_grid()
+            playerturn = 2
+            win_combos(playerturn)
+        else:
+            position = get_position(playerturn)
+            mark_position(playerturn,position)
+
+            print_grid()
+            playerturn = 1
+            win_combos(playerturn)
 
 def mark_position(playerturn, input):
     if playerturn == 1:
         board[get_row_index(input)][get_colum_index(input)] = 'X'
     else:
         board[get_row_index(input)][get_colum_index(input)] = 'O'
-
     return False
+
+
+
+def win_combos(playerturn):
+    print ("Win Combos")
+    if playerturn == 1:
+        print("Checking Player1")
+        if check_index('X'):
+            print ("Player One Wins!")
+    else:
+        if check_index('O'):
+            print ("Player Two Wins!")
+                     
+
+'''
+    elif playerturn == 2:
+        if board[0][0] == '0' and board[0][1] == '0' and board[0][2] == '0':
+            print ("Player Two Wins!")
+    else:
+        Print("No Winner")                
 '''
 
-    if board[input] == 1:
-         board[1]= 'x'
+
+def check_index(Marker):
+    print("In check Index")
+  
+    if board[0][0] == Marker and board[0][1] == Marker and board[0][2] == Marker:
+        return True
+
+    elif board[1][0] == Marker and board[1][1] == Marker and board[1][2] == Marker:
+        return True
+
+    elif board[2][0] == Marker and board[2][1] == Marker and board[2][2] == Marker:
+        return True
+               
+    elif board[0][0] == Marker and board[1][0] == Marker and board[2][0] == Marker:
+        return True
+
+    elif board[0][1] == Marker and board[1][1] == Marker and board[2][1] == Marker:
+        return True
+        
+    elif board[0][2] == Marker and board[1][2] == Marker and board[2][2] == Marker:
+        return True
+        
+    elif board[0][0] == Marker and board[1][1] == Marker and board[2][2] == Marker:
+        return True
+
+    elif board[0][2] == Marker and board[1][1] == Marker and board[2][0] == Marker:
+        return True
+        
     else:
-            print ("This position is taken, please choose a different spot"):
+        return False 
 
-# def marks(player_choice):
-    # for i in range(len(board)):
-        # for j in range(len(board[i])):
-           # print (board[i][j])
 
-            #if (board[i][j]) == ():
-                # board[i][j] = ('o')
+
+
+
+'''
+    board = [
+        ['1', '2', '3'],
+        ['4', '5', '6'],
+        ['7','8','9']
+    ]
+
+[3,5,7] 147][258][369]
+    Diagonal = 159, 357
+    Vertical = 147, 258, 369
+    Horizontal = 123, 456, 789
+
 '''
 
 
